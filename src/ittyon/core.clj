2a1a4996c87041d36aad6c331acd67c93764db60
James Reeves <jreeves@weavejester.com>
2014-05-21 01:03:00 +0100

    Add basic assert/retract functions

--- src/ittyon/core.clj
+++ src/ittyon/core.clj
@@ -1,6 +1,16 @@
-(ns ittyon.core)
+(ns ittyon.core
+  (:refer-clojure :exclude [assert]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def empty-state
+  {:snapshot #{}})
+
+(defn assert [state eavt]
+  (update-in state [:snapshot] conj eavt))
+
+(defn retract [state eavt]
+  (update-in state [:snapshot] disj eavt))
+
+(defn commit [state [o e a v t]]
+  (case o
+    :assert  (assert state [e a v t])
+    :retract (retract state [e a v t])))

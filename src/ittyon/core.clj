25b1572dcfebb11d9369bb0a4d3e407b94ee57ed
James Reeves <jreeves@weavejester.com>
2014-05-21 03:31:37 +0100

    Add indexes to state for fast lookup

--- src/ittyon/core.clj
+++ src/ittyon/core.clj
@@ -2,13 +2,30 @@
   (:refer-clojure :exclude [assert]))
 
 (def empty-state
-  {:snapshot #{}})
+  {:snapshot #{}
+   :indexes {:eavt {}, :aevt {}, :avet {}}})
 
-(defn assert [state eavt]
-  (update-in state [:snapshot] conj eavt))
+(defn dissoc-in [m [k & ks]]
+  (if (seq ks)
+    (let [v (dissoc-in (get m k) ks)]
+      (if (empty? v)
+        (dissoc m k)
+        (assoc m k v)))
+    (dissoc m k)))
 
-(defn retract [state eavt]
-  (update-in state [:snapshot] disj eavt))
+(defn assert [state [e a v t]]
+  (-> state
+      (update-in [:snapshot] conj [e a v t])
+      (assoc-in [:indexes :eavt e a v t] true)
+      (assoc-in [:indexes :aevt a e v t] true)
+      (assoc-in [:indexes :avet a v e t] true)))
+
+(defn retract [state [e a v t]]
+  (-> state
+      (update-in [:snapshot] disj [e a v t])
+      (update-in [:indexes :eavt] dissoc-in [e a v t])
+      (update-in [:indexes :aevt] dissoc-in [a e v t])
+      (update-in [:indexes :avet] dissoc-in [a v e t])))
 
 (defn commit [state [o e a v t]]
   (case o

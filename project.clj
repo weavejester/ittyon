5ada91733a2d6bbe7f8044a5ff4486c753ecac6b
James Reeves <jreeves@weavejester.com>
2014-05-22 04:16:41 +0100

    Add criterium as a dev dependency

--- project.clj
+++ project.clj
@@ -4,4 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [medley "0.2.0"]])
+                 [medley "0.2.0"]]
+  :profiles
+  {:dev {:jvm-opts ^:replace {}
+         :dependencies [[criterium "0.4.3"]]}})

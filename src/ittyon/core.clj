50a9969e132826f25afe6e37ff890546718c4f8f
James Reeves <jreeves@weavejester.com>
2014-05-22 01:19:40 +0100

    Add from-snapshot

--- src/ittyon/core.clj
+++ src/ittyon/core.clj
@@ -4,17 +4,27 @@
 
 (def empty-state
   {:snapshot #{}
-   :indexes {:eavt {}, :aevt {}, :avet {}}})
+   :indexes  {:eavt {}, :aevt {}, :avet {}}})
+
+(defn- add-eavt [i [e a v t]] (assoc-in i [e a v t] true))
+(defn- add-aevt [i [e a v t]] (assoc-in i [a e v t] true))
+(defn- add-avet [i [e a v t]] (assoc-in i [a v e t] true))
+
+(defn from-snapshot [snapshot]
+  {:snapshot (set snapshot)
+   :indexes  {:eavt (reduce add-eavt {} snapshot)
+              :aevt (reduce add-aevt {} snapshot)
+              :avet (reduce add-avet {} snapshot)}})
 
 (defn time []
   (System/currentTimeMillis))
 
-(defn assert [state [e a v t]]
+(defn assert [state eavt]
   (-> state
-      (update-in [:snapshot] conj [e a v t])
-      (assoc-in [:indexes :eavt e a v t] true)
-      (assoc-in [:indexes :aevt a e v t] true)
-      (assoc-in [:indexes :avet a v e t] true)))
+      (update-in [:snapshot] conj eavt)
+      (update-in [:indexes :eavt] add-eavt eavt)
+      (update-in [:indexes :aevt] add-aevt eavt)
+      (update-in [:indexes :avet] add-avet eavt)))
 
 (defn retract [state [e a v t]]
   (-> state

e987d09a5e035149986d15b1f63bdf6f6c5c61d1
James Reeves <jreeves@weavejester.com>
2014-06-15 00:27:58 +0100

    Update Medley to 0.3.0

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/core.async "0.1.303.0-886421-alpha"]
-                 [medley "0.2.0"]]
+                 [medley "0.3.0"]]
   :profiles
   {:dev {:jvm-opts ^:replace {}
          :source-paths ["dev"]

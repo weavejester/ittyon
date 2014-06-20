cb8cdddd05087e9e6fb1729536c8f6056af38822
James Reeves <jreeves@weavejester.com>
2014-06-20 21:06:21 +0100

    Add some basic tests

--- test/ittyon/core_test.clj
+++ test/ittyon/core_test.clj
@@ -1,7 +1,42 @@
 (ns ittyon.core-test
   (:require [clojure.test :refer :all]
-            [ittyon.core :refer :all]))
+            [ittyon.core :as i]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(def eavt-state
+  {:snapshot {[:e :a :v] :t}
+   :index {:eavt {:e {:a {:v :t}}}
+           :aevt {:a {:e {:v :t}}}
+           :avet {:a {:v {:e :t}}}}})
+
+(deftest test-from-snapshot
+  (is (= (i/from-snapshot {[:e :a :v] :t})
+         eavt-state)))
+
+(deftest test-assert
+  (is (= (i/assert i/empty-state [:e :a :v :t])
+         eavt-state)))
+
+(deftest test-revoke
+  (is (= (i/revoke eavt-state [:e :a :v :t])
+         i/empty-state)))
+
+(deftest test-time
+  (is (= (integer? (i/time)))))
+
+(deftest test-uuid
+  (is (i/uuid? (i/uuid)))
+  (is (not= (i/uuid) (i/uuid))))
+
+(deftest test-derive
+  (is (= (i/derive (make-hierarchy) ::a ::b ::c)
+         (-> (make-hierarchy)
+             (derive ::a ::b)
+             (derive ::a ::c)))))
+
+(deftest test-revision?
+  (is (not (i/revision? nil)))
+  (is (not (i/revision? [:o :e :a :v :t])))
+  (is (not (i/revision? [:assert :e "a" :v :t])))
+  (is (not (i/revision? [:assert :e :a :v])))
+  (is (i/revision? [:assert :e :a :v :t]))
+  (is (i/revision? [:assert :e :a :v :t])))

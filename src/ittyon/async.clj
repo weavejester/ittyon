59c4613fc821402055cf21761acfb1970416f9ea
James Reeves <jreeves@weavejester.com>
2014-06-21 21:26:56 +0100

    Refactor async code into client/server structures

--- src/ittyon/async.clj
+++ src/ittyon/async.clj
@@ -1,46 +1,49 @@
 (ns ittyon.async
+  (:refer-clojure :exclude [send])
   (:require [clojure.core.async :as a :refer [go go-loop <! >!]]
             [ittyon.core :as i]))
 
-(defn put-all! [chs msg]
-  (doseq [ch chs] (a/put! ch msg)))
-
-(defn close-all! [chs]
-  (doseq [ch chs] (a/close! ch)))
-
-(defn connect [sysref socket]
-  (let [input (a/chan)]
+(defn client [init]
+  (let [local  (a/chan)
+        remote (atom nil)
+        system (atom init)]
     (go-loop []
-      (let [[event port] (a/alts! [socket input])]
-        (if event
-          (do (when (identical? port input) (>! socket event))
-              (swap! sysref i/recv-client event)
-              (recur))
-          (close-all! [socket input]))))
-    (a/map>
-     (fn [[o e a v]] [:commit [o e a v (i/time @sysref)]])
-     input)))
+      (when-let [event (<! local)]
+        (some-> @remote (>! event))
+        (swap! system i/recv-client event)
+        (recur)))
+    {:system system
+     :local  local
+     :remote remote}))
 
-(defn listen [sysref sockets socket]
-  (let [buffer (a/chan 32)]
-    (go (swap! sockets conj buffer)
-        (>! socket [:time (i/time)])
-        (>! socket [:reset (-> @sysref :state :snapshot)])
-        (a/pipe buffer socket)
-        (loop []
-          (when-let [event (<! socket)]
-            (swap! sysref i/recv-server event)
-            (put-all! (disj @sockets socket) event)
-            (recur)))
-        (swap! sockets disj buffer)
-        (a/close! buffer))))
+(defn connected? [{:keys [remote]}]
+  (not (nil? @remote)))
 
-(defn acceptor [sysref]
-  (let [conn    (a/chan)
-        sockets (atom #{})]
-    (go (loop []
-          (when-let [socket (<! conn)]
-            (listen sysref sockets socket)
-            (recur)))
-        (close-all! @sockets))
-    conn))
+(defn connect [{:keys [remote system]} socket]
+  (reset! remote socket)
+  (go-loop []
+    (when-let [event (<! socket)]
+      (swap! system i/recv-client event)
+      (recur))))
+
+(defn send [{:keys [local system]} [o e a v]]
+  (a/put! local [:commit [o e a v (i/time @system)]]))
+
+(defn server [init]
+  {:system  (atom init)
+   :sockets (atom #{})})
+
+(defn broadcast [{:keys [sockets]} socket message]
+  (doseq [sock @sockets :when (not= sock socket)]
+    (a/put! sock message)))
+
+(defn accept [{:keys [sockets system] :as server} socket]
+  (go (>! socket [:time (i/time)])
+      (>! socket [:reset (-> @system :state :snapshot)])
+      (swap! sockets conj socket)
+      (loop []
+        (when-let [event (<! socket)]
+          (swap! system i/recv-server event)
+          (broadcast server socket event)
+          (recur)))
+      (swap! sockets disj socket)))
--- test/ittyon/async_test.clj
+++ test/ittyon/async_test.clj
@@ -7,26 +7,25 @@
 
 (deftest test-async
   (i/derive ::name ::i/aspect ::i/singular)
-  (let [entity     (i/uuid)
-        server-sys (atom (-> i/empty-system
-                             (i/commit [:assert entity ::i/live? true (i/time)])
-                             (i/commit [:assert entity ::name "alice" (i/time)])))
-        client-sys (atom i/empty-system)
-        server     (ia/acceptor server-sys)
-        a-ch       (a/chan)
-        b-ch       (a/chan)
-        client     (ia/connect client-sys (bidi-ch a-ch b-ch))]
-    
-    (>!! server (bidi-ch b-ch a-ch))
+  (let [entity  (i/uuid)
+        system  (-> i/empty-system
+                    (i/commit [:assert entity ::i/live? true (i/time)])
+                    (i/commit [:assert entity ::name "alice" (i/time)]))
+        server  (ia/server system)
+        client  (ia/client i/empty-system)
+        a-ch    (a/chan)
+        b-ch    (a/chan)]
+    (ia/connect client (bidi-ch a-ch b-ch))
+    (ia/accept  server (bidi-ch b-ch a-ch))
     
     (testing "initial state transferred to client"
       (Thread/sleep 25)
-      (is (= (-> @client-sys :state :snapshot)
-             (-> @server-sys :state :snapshot))))
+      (is (= (-> client :system deref :state :snapshot)
+             (-> server :system deref :state :snapshot))))
 
     (testing "client events relayed to server"
-      (>!! client [:assert entity ::name "bob"])
+      (ia/send client [:assert entity ::name "bob"])
       (Thread/sleep 25)
-      (is (= (-> @client-sys :state :snapshot keys set)
-             (-> @server-sys :state :snapshot keys set)
+      (is (= (-> client :system deref :state :snapshot keys set)
+             (-> server :system deref :state :snapshot keys set)
              #{[entity ::i/live? true], [entity ::name "bob"]})))))

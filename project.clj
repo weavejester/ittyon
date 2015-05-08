ff7e108fcb4c86bc27899a7ac685c906ab7f2ad8
James Reeves <jreeves@weavejester.com>
2015-05-08 13:49:02 +0100

    Release 0.7.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ittyon "0.6.0"
+(defproject ittyon "0.7.0"
   :description "Manage distributed state for games"
   :url "https://github.com/weavejester/ittyon"
   :license {:name "Eclipse Public License"

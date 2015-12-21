ac2ca289e4686d965ddf0f51eb36e960870f60f2
James Reeves <jreeves@weavejester.com>
2015-12-21 20:01:18 +0000

    Update ClojureScript and core.async dependencies

--- project.clj
+++ project.clj
@@ -4,11 +4,11 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]
+                 [org.clojure/core.async "0.2.374"]
                  [medley "0.7.0"]
                  [intentions "0.2.0"]]
   :plugins [[com.cemerick/clojurescript.test "0.3.3"]
-            [lein-cljsbuild "1.0.6"]
+            [lein-cljsbuild "1.1.2"]
             [codox "0.8.13"]]
   :codox {:defaults {:doc/format :markdown}
           :src-dir-uri "http://github.com/weavejester/ittyon/blob/0.10.2/"
@@ -24,7 +24,7 @@
    "test-cljs" ["cljsbuild" "test"]
    "test-all"  ["do" ["test"] ["cljsbuild" "test"]]}
   :profiles
-  {:provided {:dependencies [[org.clojure/clojurescript "0.0-3308"]]}
+  {:provided {:dependencies [[org.clojure/clojurescript "1.7.189"]]}
    :dev      {:dependencies [[org.clojure/tools.namespace "0.2.11"]
                              [criterium "0.4.3"]
                              [jarohen/chord "0.6.0"]]
